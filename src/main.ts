b60baf036bedae2308f65c33419aa2f0031cfb1a
a.salin <web.salinap@gmail.ru>
2026-05-28 11:41:11 +0500

    reset

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-  const port = configService.get<number>('app.port') ?? 3000;
-
-  await app.listen(port);
+  await app.listen(process.env.PORT ?? 3000);
 }
-void bootstrap();
+bootstrap();
